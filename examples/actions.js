35cc8f3641d6088908e45a5bd88379f5a8ee7015
Sergii Puliaiev <spuliaiev@salesforce.com>
2021-01-12 13:36:35 -0800

    draft version

---
+++ examples/actions.js
@@ -0,0 +1,218 @@
+// This example shows how to listen to a button click
+// It uses slash commands and actions
+// Require the Bolt package (github.com/slackapi/bolt)
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+// Listen for a slash command invocation
+app.command('/helloworld', async ({ ack, payload, context }) => {
+  console.log("Command received helloworld");
+  // Acknowledge the command request
+  ack();
+
+  try {
+    const result = await app.client.chat.postMessage({
+      token: context.botToken,
+      // Channel to send message to
+      channel: payload.channel_id,
+      // Include a button in the message (or whatever blocks you want!)
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: 'Go ahead. Click it.'
+          },
+          accessory: {
+            type: 'button',
+            text: {
+              type: 'plain_text',
+              text: 'Click me!'
+            },
+            action_id: 'button_abc'
+          }
+        }
+      ],
+      // Text in the notification
+      text: 'Message from Test App'
+    });
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+
+// Listen for a button invocation with action_id `button_abc`
+// You must set up a Request URL under Interactive Components on your app configuration page
+app.action('button_abc', async ({ ack, body, context }) => {
+  // Acknowledge the button request
+  console.log("Acttion received button_abc");
+  ack();
+
+  try {
+    // Update the message
+    if (body.message) {
+      console.log("Action received button_abc - updating message");
+      const result = await app.client.chat.update({
+        token: context.botToken,
+        // ts of message to update
+        ts: body.message ? body.message.ts : body.actions?body.actions[0].action_ts : 0,
+        // Channel of message
+        channel: body.channel.id,
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: '*The button was clicked!*'
+            }
+          }
+        ],
+        text: 'Message from Test App'
+      });
+      console.log(result);
+    } else {
+      console.log("Action received button_abc - message is missing");
+      const result = { "error": "Message is missing - no updates" };
+      console.log(result);
+    }
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+
+// Listen for a link_shared event invocation
+app.event('link_shared', async ({ event, client, context }) => {
+  console.log("Event received link_shared", event);
+  // console.log("Event received link_shared, Client", client);
+  // Acknowledge the command request
+
+  try {
+
+    let furls = {};
+    for (let lnkInd in event.links) {
+      let linkData = event.links[lnkInd];
+      furls[linkData.url] = {
+        "blocks": [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": `Detected domain ${linkData.domain}`
+            },
+            "accessory": {
+              "type": "image",
+              "image_url": "https://gentle-buttons.com/img/carafe-filled-with-red-wine.png",
+              "alt_text": "Stein's wine carafe"
+            }
+          },
+          {
+            "type": "actions",
+            "elements": [
+              {
+                "type": "button",
+                action_id: 'button_abc',
+                "text": {
+                  "type": "plain_text",
+                  "text": "Click me ABC!"
+                }
+              }
+            ]
+          }
+        ]
+      };
+    }
+    const result = await app.client.chat.unfurl({
+      token: context.botToken,
+      // ts of message to update
+      ts: event.message_ts,
+      // Channel of message
+      channel: event.channel,
+      "unfurls": furls
+    });
+    console.log("UNFURL:", result);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+
+app.event('app_home_opened', async ({ event, client, context }) => {
+  console.log("Event received app_home_opened");
+  try {
+    /* view.publish is the method that your app uses to push a view to the Home tab */
+    const result = await client.views.publish({
+
+      /* the user that opened your app's app home */
+      user_id: event.user,
+
+      /* the view object that appears in the app home*/
+      view: {
+        type: 'home',
+        callback_id: 'home_view',
+
+        /* body of the view */
+        blocks: [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "*Welcome to your _App's Home_* :tada:"
+            }
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+            }
+          },
+          {
+            "type": "actions",
+            "elements": [
+              {
+                "type": "button",
+                action_id: 'button_abc',
+                "text": {
+                  "type": "plain_text",
+                  "text": "Click me ABC!"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    });
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+
+// Test Actions
+/*
+app.post('/slack/actions', async(req, res) => {
+  console.log('Generic Action Received '+req.body.payload);
+  const { token, trigger_id, user, actions, type } = JSON.parse(req.body.payload);
+  if(actions && actions[0].action_id.match(/add_/)) {
+    // openModal(trigger_id);
+  }
+});
+*/
+
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app Actions is running!');
+})();
