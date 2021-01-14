f57136b492ee8d4ed9176b4669c5f264a6c8b8a6
Sergii Puliaiev <spuliaiev@salesforce.com>
2021-01-13 16:14:29 -0800

    Refactoring and cleanup

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 // Require the Bolt package (github.com/slackapi/bolt)
 const { App, ExpressReceiver } = require('@slack/bolt');
 const configReader = require("./lib/configReader")
-const commandHelloWorld = require("./lib/commandHelloWorld")
 
 // Create a Bolt Receiver
 const receiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET });
@@ -15,6 +14,7 @@ const app = new App({
 });
 
 const config = configReader.readConfig();
+
 // Other web requests are methods on receiver.router
 receiver.router.get('/', (req, res) => {
 //   You're working with an express req and res now.
@@ -23,197 +23,16 @@ receiver.router.get('/', (req, res) => {
 });
 
 // Listen for a slash command invocation
-app.command('/helloworld', async ({ ack, payload, context }) => {
-  return commandHelloWorld.process(config, ack, payload, context);
-});
 
-app.command('/helloworld22', async ({ ack, payload, context }) => {
-  console.log("Command received helloworld");
-  // Acknowledge the command request
-  await ack();
+app.command('/work', require ('./lib/gusWorkAction.js')(app, config).process);
 
-  try {
-    const result = await app.client.chat.postMessage({
-      token: context.botToken,
-      // Channel to send message to
-      channel: payload.channel_id,
-      // Include a button in the message (or whatever blocks you want!)
-      blocks: [
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: 'Go ahead. Click it.'
-          },
-          accessory: {
-            type: 'button',
-            text: {
-              type: 'plain_text',
-              text: 'Click me!'
-            },
-            action_id: 'button_abc'
-          }
-        }
-      ],
-      // Text in the notification
-      text: 'Message from Test App'
-    });
-    console.log(result);
-  }
-  catch (error) {
-    console.error(error);
-  }
-});
-
-// Listen for a button invocation with action_id `button_abc`
 // You must set up a Request URL under Interactive Components on your app configuration page
-app.action('button_abc', async ({ ack, body, context }) => {
-  // Acknowledge the button request
-  console.log("Acttion received button_abc");
-  await ack();
-
-  try {
-    // Update the message
-    if (body.message) {
-      console.log("Action received button_abc - updating message");
-      const result = await app.client.chat.update({
-        token: context.botToken,
-        // ts of message to update
-        ts: body.message.ts,
-        // Channel of message
-        channel: body.channel.id,
-        blocks: [
-          {
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: '*The button was clicked!*'
-            }
-          }
-        ],
-        text: 'Message from Test App'
-      });
-      console.log(result);
-    } else {
-      console.log("Action received button_abc - message is missing");
-      const result = { "error": "Message is missing - no updates" };
-      console.log(result);
-    }
-  }
-  catch (error) {
-    console.error(error);
-  }
-});
+app.action('button_abc', require('./lib/buttonAction.js')(app, config).process);
 
 // Listen for a link_shared event invocation
-app.event('link_shared', async ({ event, client, context }) => {
-  console.log("Event received link_shared", event);
-  // console.log("Event received link_shared, Client", client);
+app.event('link_shared', require ('./lib/linkUnfurlEvent.js')(app, config).process);
 
-  try {
-
-    let furls = {};
-    for (let lnkInd in event.links) {
-      let linkData = event.links[lnkInd];
-      furls[linkData.url] = {
-        "blocks": [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": `Detected domain ${linkData.domain}`
-            },
-            "accessory": {
-              "type": "image",
-              "image_url": "https://gentle-buttons.com/img/carafe-filled-with-red-wine.png",
-              "alt_text": "Stein's wine carafe"
-            }
-          },
-          {
-            "type": "actions",
-            "elements": [
-              {
-                "type": "button",
-                action_id: 'button_abc',
-                "text": {
-                  "type": "plain_text",
-                  "text": "Click me ABC!"
-                }
-              }
-            ]
-          }
-        ]
-      };
-    }
-    const result = await app.client.chat.unfurl({
-      token: context.botToken,
-      // ts of message to update
-      ts: event.message_ts,
-      // Channel of message
-      channel: event.channel,
-      "unfurls": furls
-    });
-    console.log("UNFURL:", result);
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-
-app.event('app_home_opened', async ({ event, client, context }) => {
-  console.log("Event received app_home_opened");
-  try {
-    /* view.publish is the method that your app uses to push a view to the Home tab */
-    const result = await client.views.publish({
-
-      /* the user that opened your app's app home */
-      user_id: event.user,
-
-      /* the view object that appears in the app home*/
-      view: {
-        type: 'home',
-        callback_id: 'home_view',
-
-        /* body of the view */
-        blocks: [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "*Welcome to your _App's Home_* :tada: in the HEROKU!!"
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
-            }
-          },
-          {
-            "type": "actions",
-            "elements": [
-              {
-                "type": "button",
-                action_id: 'button_abc',
-                "text": {
-                  "type": "plain_text",
-                  "text": "Click me ABC!"
-                }
-              }
-            ]
-          }
-        ]
-      }
-    });
-  }
-  catch (error) {
-    console.error(error);
-  }
-});
+app.event('app_home_opened', require ('./lib/appHomeEvent.js')(app, config).process);
 
 app.error(async (error) => {
   // Check the details of the error to handle cases where you should retry sending a message or stop the app
